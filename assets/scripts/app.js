c3b439b9a0226ba35a86642e0e8ac9baad4db7b7
ASHRAF K AJIDA <ashraf.ajida@yahoo.com>
2019-04-09 23:52:08 +0100

    form

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,10 +1,13 @@
 let burgerBtn = document.querySelector('.burger-menu');
 let menu = document.querySelector('.nav-links');
 let gallery = document.querySelector('.gallery');
+let sectionGallery = document.querySelector('.section-gallery');
+let modelSection = document.querySelector('.section-model');
 let model = document.querySelector('.model');
 let modelImage = document.querySelector('.model-image');
 let closeBtn = document.querySelector('.close');
-
+let btn = document.querySelector('.btn');
+let openForm = document.querySelector('.message-form');
 
 burgerBtn.addEventListener('click', function () {
     menu.classList.toggle('open');
@@ -15,13 +18,30 @@ gallery.addEventListener('click', function (e) {
 
 
     if (e.target.className === "overlay") {
-        model.style.display = "block";
+        // sectionGallery.style.background = '#000';
+        modelSection.style.display = "block";
+        model.style.opacity = 1;
         modelImage.src = imageSrc;
         modelImage.style.opacity = 1;
+
     }
 
 })
 
-closeBtn.addEventListener('click', function (e) {
-    model.style.display = "none";
+document.addEventListener('click', function (e) {
+    if (e.target.className === "section-model") {
+        modelSection.style.display = "none";
+    }
+
+    console.log(e.target.className);
 })
+
+closeBtn.addEventListener('click', function (e) {
+    modelSection.style.display = "none";
+})
+
+btn.addEventListener('click', function (event) {
+    // event.preventDefault();
+    console.log('clicked');
+    openForm.classList.toggle('form-open');
+});
